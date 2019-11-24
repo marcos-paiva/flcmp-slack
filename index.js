e69fc3f6b209d236091421732a241a864c18b4b7
Marcos <marcos@nuvemshop.com.br>
2019-11-24 18:23:10 -0300

    Add first api message

--- index.js
+++ index.js
@@ -19,4 +19,43 @@ bot.on('start', () => {
         '',
         params
     );
+
 })
+
+bot.on('error', (error) => {
+    console.log(error)
+});
+
+bot.on('message', (data) => {
+    if(data.type !== 'message') {
+        return;
+    }
+    handleMessage(data.text);
+})
+
+function handleMessage(message) {
+    if (message.includes(' go')) {
+        randomJoke();
+    } else {
+        getHelp();
+    }
+}
+
+const api = axios.create({
+    baseURL: 'http://localhost:3001/api/phrases'
+});
+
+async function randomJoke() {
+    try {
+        const res = await api.get();
+        bot.postMessageToChannel('integração', res.data);
+        console.info(res.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function getHelp() {
+
+}
+
