ebc689e9dd54457d465597ca1bf4b1ebc1d47476
Marcos <marcos@nuvemshop.com.br>
2019-12-29 01:58:21 -0300

    build a server

--- index.js
+++ index.js
@@ -1,65 +1,9 @@
-const SlackBot = require('slackbots');
-const axios = require('axios');
-const dotEnv = require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-const bot = new SlackBot({
-    token: `${process.env.BOT_TOKEN}`,
-    name: 'falacampeao'
-});
+app.use(cors());
+app.use('/api', require('./src/routes'));
 
-bot.on('start', () => {
-    const params = {
-        icon_emoji: ':robot_face:'
-    }
-
-    bot.postMessageToChannel(
-        'integração',
-        '',
-        params
-    );
-})
-
-bot.on('error', (error) => {
-    console.log(error)
-});
-
-bot.on('message', (data) => {
-    if(data.type !== 'message') {
-        return;
-    }
-    console.log('User ID: ' + data.user + ' - User message: ' + data.text );
-    //handleMessage(data.text);
-})
-
-// function handleMessage(message) {
-//     if (message.includes(' go')) {
-//         randomJoke();
-//     } else {
-//         getHelp();
-//     }
-// }
-
-// const api = axios.create({
-//     baseURL: 'http://localhost:3001/api/phrases'
-// });
-
-// async function randomJoke() {
-//     try {
-//         const res = await api.get();
-//         bot.postMessageToChannel('integração', res.data);
-//         console.info(res.data);
-//         bot.prependListener()
-//         console.log(bot.getUserId());
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
-
-// // bot.on('message', function(data) {
-// //     // all ingoing events https://api.slack.com/rtm
-// // });
-
-// function getHelp() {
-
-// }
-
+let port = process.env.PORT || 3001;
+app.listen(port);
