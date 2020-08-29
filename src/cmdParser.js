043ff4630e85aca7d215139634422fa266787285
Marcos <marcos@nuvemshop.com.br>
2020-08-29 10:57:34 -0300

    Update cmdParser.js

--- src/cmdParser.js
+++ src/cmdParser.js
@@ -1,15 +1,23 @@
 const getPhrases = require('./getPhrases')
 
-async function loadPhrases() {
-    const response = await getPhrases.get();
-    return response.data;
-}
-
 const cmdParser = (userCommand) => {
-    if (userCommand.text == 'rand') {
-       loadPhrases();
+    if (userCommand.text === 'rand') {
+
+        getPhrases.get().then(function(response){
+            const responseObj = {
+                response_type: userCommand.channel_id,
+                text: response.data
+            }
+            return responseObj;
+        }).catch(function (error) {
+            console.log(error);
+        })
+
     } else {
+
+        console.log('Command not found');
         return "Command unknown";
+
     }
 }
 
